ffab1cdebf5ce3cb20a8f32edea54afcce5feb03
aceazad1992 <aceazad@hotmail.com>
2022-09-30 12:16:00 +0100

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+import pytest
+
+def test_user_input_number():
+    user_input = '12'
+    assert int(user_input)
+
+
+def test_user_input_number():
+    user_input = '5'
+    assert int(user_input)
+
+
+def test_user_input_number():
+    user_input = '7'
+    assert int(user_input)
+
+
+def test_user_input_number():
+    user_input = '100'
+    assert int(user_input)
